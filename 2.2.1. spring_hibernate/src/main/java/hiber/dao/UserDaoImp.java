2a3d4a1d5f9b3cb0bb31896fab3bc6914c4e1023
Vladislav Bachagov <vlad@bachagov.ru>
2025-02-27 23:22:38 +0300

    2_2_1- Убрал каскады, связал бины через конструктор, оптимизировал методы listUsers и findUserByCar

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,25 +11,36 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
+    private final SessionFactory sessionFactory;
+
     @Autowired
-    private SessionFactory sessionFactory;
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void add(User user) {
+        if (user.getCar() != null && user.getCar().getId() == null) {
+            sessionFactory.getCurrentSession().save(user.getCar());
+        }
         sessionFactory.getCurrentSession().save(user);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
+                "SELECT u FROM User u " +
+                        "LEFT JOIN FETCH u.car", User.class);
         return query.getResultList();
     }
 
     @Override
     public User findUserByCar(String model, int series) {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User user" +
-                        " where user.car.model = :model and user.car.series = :series")
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
+                        "SELECT u FROM User u " +
+                                "JOIN FETCH u.car " +
+                                "WHERE u.car.model = :model AND u.car.series = :series", User.class)
                 .setParameter("model", model)
                 .setParameter("series", series);
         return query.getSingleResult();
